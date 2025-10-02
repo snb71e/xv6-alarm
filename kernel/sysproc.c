56f1d1eb487237cc3eaf248dd93d53e7377c023c
snb71e <seonae@dgist.ac.kr>
2025-10-02 16:24:30 +0000

    kernel/sysproc.c: implement sigalarm and sigreturn system calls

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,29 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_sigalarm(void) {
+  int ticks;
+  uint64 handler;
+  if(argint(0, &ticks) < 0) return -1;
+  if(argaddr(1, &handler) < 0) return -1;
+
+  struct proc *p = myproc();
+  p->alarm_interval = ticks;
+  p->alarm_ticks_left = ticks;
+  p->alarm_handler = handler;
+  return 0;
+}
+
+uint64
+sys_sigreturn(void) {
+  struct proc *p = myproc();
+
+  *(p->trapframe) = p->alarm_tf_backup;
+  p->trapframe->a0 = 0;
+
+  p->alarm_ticks_left = p->alarm_interval;
+  p->in_alarm = 0;
+  return 0;
+}
