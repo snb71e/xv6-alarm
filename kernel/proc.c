a85178c4edda4bdea6486f2915599204e4f52b58
snb71e <seonae@dgist.ac.kr>
2025-10-02 16:22:07 +0000

    kernel/proc.c: initialize alarm management fields in allocproc

--- kernel/proc.c
+++ kernel/proc.c
@@ -119,11 +119,18 @@ allocproc(void)
       release(&p->lock);
     }
   }
+
+  
   return 0;
 
 found:
   p->pid = allocpid();
   p->state = USED;
+  
+  p->alarm_interval = 0;
+  p->alarm_ticks_left = 0;
+  p->alarm_handler = 0;
+  p->in_alarm = 0;
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
